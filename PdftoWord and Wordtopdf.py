c99132d6a9bbf4a3b651ab2c60b0553803189003
Waqasunnabi <119784909+Waqasunnabi@users.noreply.github.com>
2023-10-20 16:57:00 +0500

    Update PdftoWord and Wordtopdf.py

--- PdftoWord and Wordtopdf.py
+++ PdftoWord and Wordtopdf.py
@@ -1,1 +1,29 @@
+import os
+from pdf2docx import parse
+from docx2pdf import convert
+
+
+folder_path_meps = r'link'
+excel_files_meps = [f for f in os.listdir(folder_path_meps) if f.endswith('.docx')]
+
+
+for files in excel_files_meps:
+    file = folder_path_meps + "\\" + files
+    docxfile = file[0:-4] + ".pdf"
+    convert(file, docxfile)
+
+
+# import os
+# from pdf2docx import parse
+
+
+# folder_path_meps = r'Link'
+# excel_files_meps = [f for f in os.listdir(folder_path_meps) if f.endswith('.pdf')]
+
+
+# for files in excel_files_meps:
+#     file = folder_path_meps + "\\" + files
+#     docxfile = file[0:-3] + ".docx"
+#     parse(file, docxfile)
+
 
